a7c101c6a69b77a4fdb4935f0464175a23e9036e
Ben <ben.williams@noaa.gov>
2023-02-07 17:52:39 -0900

    mo speed

--- R/al_variab.R
+++ R/al_variab.R
@@ -9,20 +9,7 @@
 al_variab <- function(age_dat) {
   
   age_dat %>% 
-    tidytable::mutate.(id = .I) -> age_dat
-  
-  age_dat %>% 
-    tidytable::count.(age, species_code) -> .sample_sz
-  
-  age_dat %>% 
-    tidytable::left_join.(.sample_sz) %>% 
-    tidytable::select(species_code, age, length, n) %>%
-    dplyr::group_by(age, species_code) %>% 
-    dplyr::summarise(new_length = sample(length, size = mean(n), replace = T)) %>% 
-    tidytable::left_join.(age_dat) %>% 
-    dplyr::group_by(id) %>% 
-    dplyr::slice_sample(n = 1) %>% 
-    tidytable::select.(-length, -id) %>% 
-    tidytable::rename.('length' = new_length)
+    tidytable::mutate(length = sample(length, .N, replace = TRUE), 
+                        .by = c(age, species_code))
 
 }
