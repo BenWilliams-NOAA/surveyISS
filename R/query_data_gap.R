53e2da6d60c7b95208281c5c37d1cfa28ad8ad36
pete-hulson <pete.hulson@noaa.gov>
2024-02-28 09:40:56 -0900

    organize sql scripts into subfolders

--- R/query_data_gap.R
+++ R/query_data_gap.R
@@ -27,7 +27,7 @@ query_data_gap <- function(survey, region, species, yrs = NULL, database, userna
   
   # length frequency data ----
   # lfreq = sql_read('length_freq_gap.sql')
-  lfreq = readLines(here::here('inst', 'sql', 'length_freq_gap.sql'))
+  lfreq = readLines(here::here('inst', 'sql', 'gap_products', 'length_freq_gap.sql'))
   lfreq = sql_filter(sql_precode = "IN", x = survey, sql_code = lfreq, flag = '-- insert survey')
   lfreq = sql_filter(sql_precode = "IN", x = species, sql_code = lfreq, flag = '-- insert species')
   lfreq = sql_filter(sql_precode = ">=", x = yrs, sql_code = lfreq, flag = '-- insert year')
@@ -39,7 +39,7 @@ query_data_gap <- function(survey, region, species, yrs = NULL, database, userna
   
   # specimen data ----
   # sp = sql_read('specimen_gap.sql')
-  sp = readLines(here::here('inst', 'sql', 'specimen_gap.sql'))
+  sp = readLines(here::here('inst', 'sql', 'gap_products', 'specimen_gap.sql'))
   sp = sql_filter(sql_precode = "IN", x = survey, sql_code = sp, flag = '-- insert survey')
   sp = sql_filter(sql_precode = "IN", x = species, sql_code = sp, flag = '-- insert species')
   sp = sql_filter(sql_precode = ">=", x = yrs, sql_code = sp, flag = '-- insert year')
@@ -52,7 +52,7 @@ query_data_gap <- function(survey, region, species, yrs = NULL, database, userna
   
   # cpue data ----
   # cp = sql_read('cpue_gap.sql')
-  cp = readLines(here::here('inst', 'sql', 'cpue_gap.sql'))
+  cp = readLines(here::here('inst', 'sql', 'gap_products', 'cpue_gap.sql'))
   cp = sql_filter(sql_precode = "IN", x = survey, sql_code = cp, flag = '-- insert survey')
   cp = sql_filter(sql_precode = "IN", x = species, sql_code = cp, flag = '-- insert species')
   cp = sql_filter(sql_precode = ">=", x = yrs, sql_code = cp, flag = '-- insert year')
@@ -64,7 +64,7 @@ query_data_gap <- function(survey, region, species, yrs = NULL, database, userna
 
   # strata data ----
   # st = sql_read('strata_gap.sql')
-  st = readLines(here::here('inst', 'sql', 'strata_gap.sql'))
+  st = readLines(here::here('inst', 'sql', 'gap_products', 'strata_gap.sql'))
   st = sql_filter(sql_precode = "IN", x = survey, sql_code = st, flag = '-- insert survey')
   
   sql_run(conn, st) %>% 
@@ -77,7 +77,7 @@ query_data_gap <- function(survey, region, species, yrs = NULL, database, userna
   
   # sizecomp ----
   # lpop = sql_read('lpop_gap.sql')
-  lpop = readLines(here::here('inst', 'sql', 'lpop_gap.sql'))
+  lpop = readLines(here::here('inst', 'sql', 'gap_products', 'lpop_gap.sql'))
   lpop = sql_filter(sql_precode = "IN", x = survey, sql_code = lpop, flag = '-- insert survey')
   lpop = sql_filter(sql_precode = "IN", x = species, sql_code = lpop, flag = '-- insert species')
   lpop = sql_filter(sql_precode = ">=", x = yrs, sql_code = lpop, flag = '-- insert year')
@@ -89,7 +89,7 @@ query_data_gap <- function(survey, region, species, yrs = NULL, database, userna
   
   # agecomp ----
   # apop = sql_read('apop_gap.sql')
-  apop = readLines(here::here('inst', 'sql', 'apop_gap.sql'))
+  apop = readLines(here::here('inst', 'sql', 'gap_products', 'apop_gap.sql'))
   apop = sql_filter(sql_precode = "IN", x = survey, sql_code = apop, flag = '-- insert survey')
   apop = sql_filter(sql_precode = "IN", x = species, sql_code = apop, flag = '-- insert species')
   apop = sql_filter(sql_precode = ">=", x = yrs, sql_code = apop, flag = '-- insert year')
@@ -101,7 +101,7 @@ query_data_gap <- function(survey, region, species, yrs = NULL, database, userna
   
   # species names ----
   # .s = sql_read('species_gap.sql')
-  .s = readLines(here::here('inst', 'sql', 'species_gap.sql'))
+  .s = readLines(here::here('inst', 'sql', 'gap_products', 'species_gap.sql'))
   .s = sql_filter(sql_precode = "IN", x = species, sql_code = .s, flag = '-- insert species')
   sql_run(conn, .s) %>% 
     dplyr::rename_all(tolower) %>% 
