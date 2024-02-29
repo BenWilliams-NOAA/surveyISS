d062f49422f78a4afafecb0ed346948b3d0d13e0
pete-hulson <pete.hulson@noaa.gov>
2024-02-28 15:23:48 -0900

    adjust lpop fcn to gap_products data

--- R/lpop.R
+++ R/lpop.R
@@ -16,7 +16,7 @@ lpop <- function(lcomp, cpue, lngs) {
   # id hauls without lengths
   # see issue #35 for reasoning 
   cpue %>%
-    tidytable::drop_na(catchjoin) %>% 
+    tidytable::filter(numcpue > 0) %>% 
     tidytable::distinct(hauljoin, species_code)  %>% 
     tidytable::anti_join(lcomp %>%
                             tidytable::summarise(hauljoin = unique(hauljoin),
@@ -47,69 +47,11 @@ lpop <- function(lcomp, cpue, lngs) {
       tidytable::filter(!is.na(sex)) -> .temp
   }
   
+  # get annual pop'n @ length
   .temp %>%
-    tidytable::filter(sex == 3) %>%
-    tidytable::count(c(year, species_code), name = 'n') -> .sex_cnt_sz
-  
-  if("stratum" %in% names(lngs)){
-    .temp %>%
-      tidytable::summarise(abund = sum(sz_pop, na.rm = T), 
-                            .by = c(year, species_code, stratum, length, sex)) %>%
-      tidytable::pivot_wider(names_from = sex, values_from = abund) %>%
-      tidytable::left_join(lngs, .) %>%
-      tidytable::mutate(across(.cols = c(`1`, `2`, `3`), ~tidytable::replace_na(.x, 0))) %>%
-      tidytable::drop_na() -> .temp2
-    
-    if(length(.sex_cnt_sz$n)>0){
-      .temp2 %>% 
-        tidytable::select(year, species_code, stratum, length, males = `1`, females = `2`, unsexed = `3`) 
-    } else{
-      .temp2 %>% 
-        tidytable::select(year, species_code, stratum, length, males = `1`, females = `2`) %>%
-        tidytable::mutate(unsexed = 0) 
-    }
-  } else {
-    if(length(.sex_cnt_sz$n)>0){
-      if(length(unique(.temp$sex)) > 1){
-        .temp %>%
-          tidytable::summarise(abund = sum(sz_pop, na.rm = T), 
-                                .by = c(year, species_code, length, sex, type)) %>%
-          tidytable::pivot_wider(names_from = sex, values_from = abund) %>%
-          tidytable::left_join(lngs, .) %>%
-          tidytable::mutate(across(.cols = c(`1`, `2`, `3`), ~tidytable::replace_na(.x, 0))) %>%
-          tidytable::drop_na() %>% 
-          tidytable::select(year, species_code, length, males = `1`, females = `2`, unsexed = `3`, type)
-      } else{
-        .temp %>%
-          tidytable::summarise(abund = sum(sz_pop, na.rm = T), 
-                                .by = c(year, species_code, length, sex, type)) %>%
-          tidytable::pivot_wider(names_from = sex, values_from = abund) %>%
-          tidytable::left_join(lngs, .) %>%
-          tidytable::mutate(across(.cols = c(`1`), ~tidytable::replace_na(.x, 0))) %>%
-          tidytable::drop_na() %>% 
-          tidytable::select(year, species_code, length, total = `1`, type)
-      }
-    } else{
-      if(length(unique(.temp$sex)) > 1){
-        .temp %>%
-          tidytable::summarise(abund = sum(sz_pop, na.rm = T), 
-                                .by = c(year, species_code, length, sex, type)) %>%
-          tidytable::pivot_wider(names_from = sex, values_from = abund) %>%
-          tidytable::left_join(lngs, .) %>%
-          tidytable::mutate(across(.cols = c(`1`, `2`), ~tidytable::replace_na(.x, 0))) %>%
-          tidytable::drop_na() %>%  
-          tidytable::select(year, species_code, length, males = `1`, females = `2`, type) %>%
-          tidytable::mutate(unsexed = 0) 
-      } else{
-        .temp %>%
-          tidytable::summarise(abund = sum(sz_pop, na.rm = T), 
-                                .by = c(year, species_code, length, sex, type)) %>%
-          tidytable::pivot_wider(names_from = sex, values_from = abund) %>%
-          tidytable::left_join(lngs, .) %>%
-          tidytable::mutate(across(.cols = c(`1`), ~tidytable::replace_na(.x, 0))) %>%
-          tidytable::drop_na() %>%  
-          tidytable::select(year, species_code, length, total = `1`, type) 
-      }
-    }
-  }
+    tidytable::summarise(abund = sum(sz_pop, na.rm = T), 
+                         .by = c(year, species_code, length, sex, type)) %>%
+    tidytable::left_join(lngs, .) %>%
+    tidytable::drop_na()
+
 }
