67dfad48ccc77966a412e07d7c7ad4221010c842
pete-hulson <pete.hulson@noaa.gov>
2024-03-11 10:32:41 -0800

    fix hauljoin bug in lpop

--- R/lpop.R
+++ R/lpop.R
@@ -27,7 +27,7 @@ lpop <- function(lcomp, cpue, lngs) {
   cpue %>%
     tidytable::mutate(st_num = mean(numcpue) * area,
                        tot = sum(numcpue), 
-                       .by = c(year, species_code, stratum)) %>%
+                       .by = c(year, species_code, stratum, hauljoin)) %>%
     tidytable::summarise(abund = mean(numcpue) / tot * st_num,
                           .by = c(year, species_code, stratum, hauljoin)) -> .pop
   
