c7798cfb82334f45b7b6581e43ff091dcb6fda6c
pete-hulson <pete.hulson@noaa.gov>
2024-05-10 09:31:59 -0800

    change agrument for global in gapindex - gap_products test

--- dev/gap_check/dev/gap_prod_indx_test.R
+++ dev/gap_check/dev/gap_prod_indx_test.R
@@ -37,7 +37,7 @@ gap_lc <- gapindex::calc_sizecomp_stratum(gapdata,
 # get age-length key
 alk <- gapindex::calc_alk(gapdata,
                           unsex = "all",
-                          global = FALSE)
+                          global = TRUE)
 
 # get stratum-level age pop'n
 gap_age_comp_st <- gapindex::calc_agecomp_stratum(gapdata,
