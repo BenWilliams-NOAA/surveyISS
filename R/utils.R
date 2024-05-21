222f6966c72fd9f14df50d382fb675b6c362777c
pete-hulson <pete.hulson@noaa.gov>
2024-05-21 15:43:02 -0800

    remove utils.R

--- R/utils.R
+++
@@ -1,5 +0,0 @@
-#'
-core_samp <- function(data, samp, grp = c('year', 'species_code', 'stratum', 'hauljoin'), replace = FALSE) {
-  data %>%
-    .[,.SD[base::sample.int(.N, min(samp,.N), replace = replace)], by = grp]
-}
