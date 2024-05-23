da49114f33c366d64ff109eb68c5480e24609f54
pete-hulson <pete.hulson@noaa.gov>
2024-05-23 14:19:15 -0800

    add data.R

---
+++ R/data.R
@@ -0,0 +1,12 @@
+#' Age and growth lab reader-tester paired otolith data
+#'
+#' A dataset containing duplicate reads of same otolith by different readers
+#'
+#' @format A data frame with 212158 observations and 4 variables:
+#' \describe{
+#' \item{species_code}{RACE species idenfication number}
+#' \item{region}{survey region: AI, BS, GOA}
+#' \item{age}{original reader age}
+#' \item{test_age}{duplicated test age from different reader}
+#' }
+"read_test"
