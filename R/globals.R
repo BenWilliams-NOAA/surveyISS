72db136fdb6b1a37d9c899e9dc5070bb91b53504
Ben Williams <ben.williams@noaa.gov>
2026-08-07 10:49:27 -0800

    spelling/pass check

---
+++ R/globals.R
@@ -0,0 +1,15 @@
+utils::globalVariables(c(
+  ".", "S_ijklm", "S_ik", "S_iklm", "abund", "age", "age_frac", 
+  "age_num", "aged", "agepop", "area", "area_id", "area_km2", 
+  "area_type", "bias", "caal", "common_name", "count", "cpue_nokm2", 
+  "description", "design_year", "distance_fished_km", "hauljoin", 
+  "id", "iss", "lat_mid", "latitude_dd_end", "latitude_dd_start", 
+  "length_mm", "length_samples", "lengthed", "long_mid", 
+  "longitude_dd_end", "longitude_dd_start", "mean_length", 
+  "net_width_m", "new_age", "new_length", "nhauls", "number", 
+  "numcpue", "og_abund", "og_agepop", "og_caal", "p_a", "p_ijklm", 
+  "p_l", "p_og", "p_sim", "region", "rss", "s_ijk", "samp_length", 
+  "sd_length", "sex", "sex_desc", "sex_ln", "sex_ln_ag", "sim", 
+  "species_code", "species_name", "st_num", "strata_data", 
+  "stratum", "subarea_name", "survey_definition_id", "test_age", "tot"
+))
--- R/surveyISS-package.R
+++ R/surveyISS-package.R
@@ -4,5 +4,6 @@
 ## usethis namespace: start
 #' @import data.table
 #' @import afscdata
+#' @importFrom stats frequency rmultinom sd
 ## usethis namespace: end
 NULL
